66e1616fcfd057ac94ecbecc9d013f11a3f6feda
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-16 11:39:48 +0600

    booking confirm

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 // middleware
-
 app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
-
-// mongodb code 
+console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mehedi15.lrak9tg.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -32,17 +27,16 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // mongodb database to server
-        const serviceCollection = client.db('carDoctor').collection('services');
-        // order data store
-        const bookingCollection = client.db('carDoctor').collection('booking');
 
-        app.get('/services',async(req,res) => {
+        const serviceCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
+
+        app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
-            res.send(result)
-
+            res.send(result);
         })
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
@@ -55,33 +49,47 @@ async function run() {
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
         })
-       
-        // booking data show ui 
-        app.get('/bookings',async(req,res) =>{
+
+
+        // bookings 
+        app.get('/bookings', async (req, res) => {
+            console.log(req.query.email);
             let query = {};
-            if (req.query?.email){
-                query = {email: req.query.email}
+            if (req.query?.email) {
+                query = { email: req.query.email }
             }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
         })
-        // bookings
 
-        app.post('/bookings',async(req,res) => {
-            const booking= req.body;
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
             console.log(booking);
-
             const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedBooking = req.body;
+            console.log(updatedBooking);
+            const updateDoc = {
+                $set: {
+                    status: updatedBooking.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
 
-        // delete data 
-        app.delete('/bookings/:id', async(req,res) => {
+        app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await bookingCollection.deleteOne(query);
             res.send(result);
         })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -95,11 +103,10 @@ run().catch(console.dir);
 
 
 
-
 app.get('/', (req, res) => {
-    res.send('Doctor is running')
+    res.send('doctor is running')
 })
 
 app.listen(port, () => {
-    console.log(`Car Doctor is running on port ${port}`);
+    console.log(`Car Doctor Server is running on port ${port}`)
 })
