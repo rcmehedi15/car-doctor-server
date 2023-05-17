8ba30f01b907e4b2f7cc4913b825f891a9897bd1
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-17 11:50:43 +0600

    done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 console.log(process.env.DB_PASS)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mehedi15.lrak9tg.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
